0fe0f7a3fd44f8681628ace8968a41a32edf98ce
Vivian McNaughton <vmcnaughton@illinoislegalaid.org>
2026-08-04 17:15:29 -0500

    Implemented July 30 2026 feedback

---
+++ docassemble/JuvenileExpungement/is_eligible.py
@@ -0,0 +1,9 @@
+def is_eligible(arrest):
+  if arrest.after_arrest == "court" and arrest.at_court == "other":
+    if arrest.found_guilty == "other" or arrest.found_guilty == "traffic" or arrest.found_guilty == "murder":
+      return False
+    elif arrest.found_guilty == "felony_or_a" and (arrest.must_register or not arrest.has_ended or arrest.case_end_date > today().minus(years=2)):
+      return False
+  elif (arrest.after_arrest == "successful_supervision" or arrest.after_arrest == "failed_supervision") and arrest.must_register:
+    return False
+  return True
