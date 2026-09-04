64e8f069e77d224363e8747651984de5c69470b7
Vivian McNaughton <vmcnaughton@illinoislegalaid.org>
2026-09-04 12:05:03 -0500

    Implemented Sep 3 2026 feedback

--- docassemble/JuvenileExpungement/is_eligible.py
+++ docassemble/JuvenileExpungement/is_eligible.py
@@ -1,11 +1,12 @@
 from docassemble.base.util import today
 
 def is_eligible(arrest):
-  if arrest.after_arrest == "court" and arrest.at_court == "other":
-    if arrest.found_guilty == "other" or arrest.found_guilty == "traffic" or arrest.found_guilty == "murder":
+  if arrest.after_arrest == "court":
+    if arrest.at_court == "delinquent":
+      if arrest.found_guilty == "traffic" or arrest.found_guilty == "murder":
+        return False
+      elif arrest.found_guilty == "felony_or_a" and (arrest.must_register or not arrest.has_ended or arrest.case_end_date > today().minus(years=2)):
+        return False
+    elif (arrest.at_court == "successful_supervision" or arrest.at_court == "failed_supervision") and arrest.must_register:
       return False
-    elif arrest.found_guilty == "felony_or_a" and (arrest.must_register or not arrest.has_ended or arrest.case_end_date > today().minus(years=2)):
-      return False
-  elif (arrest.after_arrest == "successful_supervision" or arrest.after_arrest == "failed_supervision") and arrest.must_register:
-    return False
   return True
